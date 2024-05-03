eb4be7b4fb90304936864dbf5412c372c320df0a
duonglong0808 <165501031+duonglong0808@users.noreply.github.com>
2024-05-03 14:32:57 +0700

    update env

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,13 @@ const nextConfig = {
     API_GAME_DICE: 'http://localhost:9991/api',
     HOST_KU: 'https://vn.vc3559k.net/api',
   },
+
+  // Production environment
+  // env: {
+  //   API_URL: 'https://api.vk169.net/api',
+  //   API_GAME_DICE: 'https://game.api.vk169.net/api',
+  //   HOST_KU: 'https://vn.vc3559k.net/api',
+  // },
   images: {
     domains: ['storage.googleapis.com'],
   },
